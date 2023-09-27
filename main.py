98462071adf58977e612276271d9d46c98d794b5
jackconryan <58440324+jackconryan@users.noreply.github.com>
2023-09-27 16:36:02 +0100

    darts- implemented threw first advantage

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ matches = pd.read_csv('C:/Users/Jack Ryan/Documents/VSProjects/Darts/Coolmoyne L
 
 ##  populate a list of player objects 
 def fill_players(list:list):
-    for index, row in rankings.iterrows():
+    for _, row in rankings.iterrows():
         list.append(pl.Player(row['Name'], row['Rating']))
     return list
+
+def print_rankings():
+    for player in players:
+        print(f"{player.name}:{player.rating}")
 
 ##  calculate the percentage chance player_a should win a match against an oppenent player_b
 ##  return percentage chance
@@ -29,7 +33,8 @@ def calc_prev_match_data():
     for _, match in matches.iterrows():
         winner_name = match['Winner']
         loser_name = match['Loser']
-        
+        threw_first = match['Threw First']
+        winner, loser = None,None   #redeclare each loop to prevent entirely empty row in csv from creating an error
         for player in players:
             if winner_name == player.name:
                 winner = player
@@ -37,13 +42,19 @@ def calc_prev_match_data():
             if loser_name == player.name:
                 loser = player
                 continue
-        match_result(winner, loser)
+        if(winner.name ==  threw_first):
+            match_result(winner, loser, True)
+        elif(loser.name == threw_first):
+            match_result(winner, loser, False)
+        else:
+            print(f"Error matching a players name with Threw First as entered in csv {winner.name}, {loser.name}, {threw_first}")
 
 ##  calculate match result based on players name only
 ##  this means we must look up player in list of players and is 
 ##  more resource intensive 
 ##  should always use match_result() instead in application
-def match_result_name(winner_name, loser_name):
+##  DEPRECATED
+def match_result_name(winner_name, loser_name, threw_first_name):
     for player in players:
             if winner_name == player.name:
                 winner = player
@@ -51,29 +62,51 @@ def match_result_name(winner_name, loser_name):
             if loser_name == player.name:
                 loser = player
                 continue
-    match_result(winner, loser)
+    if(winner.name ==  threw_first_name):
+        match_result(winner, loser, True)
+    elif(loser.name == threw_first_name):
+        match_result(winner, loser, False)
+    else:
+        print("Error matching a players name with Threw First name")
 
 ##  match result function. takes two players objects player a is the winner and b is the loser
-# ratings must be calculated and updated  
-def match_result(player_a: pl.Player, player_b: pl.Player):
-    player_a_update, player_b_update = new_rating(player_a.rating, player_b.rating)
-    player_a.update_rating(player_a_update)
-    player_b.update_rating(player_b_update)
+##  adjust the ratings of the player who threw first by 50 elo points to account for the advatage throwing first gives a player
+##  ratings must be calculated and updated  
+
+def match_result(player_a: pl.Player, player_b: pl.Player, winner_threw_first:bool):
+    if winner_threw_first:
+        player_a_update, player_b_update = new_rating(player_a.rating+20, player_b.rating)      # add 20 elo points for first throw advatage
+        player_a.update_rating(player_a_update-20)                                              # remove 20 elo points to account for advatage given
+        player_b.update_rating(player_b_update)
+    else:
+        player_a_update, player_b_update = new_rating(player_a.rating, player_b.rating+20)      # add 20 elo points for first throw advatage
+        player_a.update_rating(player_a_update)
+        player_b.update_rating(player_b_update-20)                                              # remove 20 elo points to account for advatage given
 
 ##  update both players elo after completing a doubles match 
 ##  takes 4 player obj, averages their elo then does the usual calculation
-def doubles_match_results(player_a:pl.Player, player_b: pl.Player, player_c:pl.Player, player_d: pl.Player):
+def doubles_match_results(player_a:pl.Player, player_b: pl.Player, player_c:pl.Player, player_d: pl.Player, threw_first_name):
     winner_avg_elo = (player_a.rating + player_b.rating)/2
     loser_avg_elo = (player_c.rating + player_d.rating)/2
-    winner_update, loser_update = new_rating(winner_avg_elo, loser_avg_elo)
-    winner_diff = winner_update - winner_avg_elo
-    loser_diff = loser_update - loser_avg_elo
     
+    if  threw_first_name==player_a.name or threw_first_name==player_b.name:         ##check if winning team threw first
+        print(f"Team {player_a.name} and {player_b.name} won their doubles game. {threw_first_name} threw first")
+        winner_update, loser_update = new_rating(winner_avg_elo+20, loser_avg_elo)      ##add 20 elo to team who threw first
+        winner_diff = (winner_update-20) - winner_avg_elo                               ##remove 20 added
+        loser_diff = loser_update - loser_avg_elo
+    elif threw_first_name == player_c.name or threw_first_name==player_d.name :
+        print(f"Team {player_a.name} and {player_b.name} won. On the losing team {threw_first_name} threw first")
+        winner_update, loser_update = new_rating(winner_avg_elo, loser_avg_elo+20)      ##add 20 elo to threw first team
+        winner_diff = winner_update - winner_avg_elo
+        loser_diff = (loser_update-20) - loser_avg_elo                                  ##remove 20 added
+    else:
+        print("Doubles match issue: couldnt find player name in list of players")
+
     print(f"After their doubles game {player_a.name} and {player_b.name} gained {winner_diff} elo points")
     player_a.update_rating(player_a.rating + winner_diff),player_b.update_rating(player_b.rating + winner_diff)
     player_c.update_rating(player_c.rating + loser_diff),player_d.update_rating(player_d.rating + loser_diff)
 
-def update_csv():
+def update_rank_csv():
     player_data = [{'Name': player.name, 'Rating': player.rating} for player in players]
     player_df = pd.DataFrame(player_data)
     player_df.to_csv('C:/Users/Jack Ryan/Documents/VSProjects/Darts/Coolmoyne Leaderboard Updated Rank.csv')
@@ -81,19 +114,17 @@ players = []
 ##  fill players from excel sheet
 fill_players(players)
 
-##  calculate elo based on matches in excel sheet
+##  calculate elo based on matches stored
 calc_prev_match_data()
-
-##  print rankings
 print("After calculating the historical matches the current standings are")
-for player in players:
-    print(f"{player.name}:{player.rating}")
+print_rankings()
 
-# test doubles match
-doubles_match_results(players[3], players[4], players[0], players[1])
+##  test doubles match winner threw first
+doubles_match_results(players[3], players[4], players[0], players[1], players[3].name)
+##  test doubles match loser threw first 
+doubles_match_results(players[3], players[0], players[2], players[1], players[1].name)
 
 ##  print rankings after doubles match
-for player in players:
-    print(f"{player.name}:{player.rating}")
+print_rankings()
 
-update_csv()
+update_rank_csv()
